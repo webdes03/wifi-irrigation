ca45f0f97ea9ae22802128ba5208c814b1a046d3
Michael Greene <mike@mike-greene.com>
2018-06-12 15:47:39 -0400

    Readme

--- wifi-irrigation.ino
+++ wifi-irrigation.ino
@@ -1,38 +1,37 @@
 /*
-
 wifi-irrigation project for Particle Photon
 Copyright 2018 Michael Greene
 https://mike-greene.com/wifi-irrigation
 
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use this 
 file except in compliance with the License. You may obtain a copy of the License at:
-ttp://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
         
 Unless required by applicable law or agreed to in writing, software distributed under 
 the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF 
 ANY KIND, either express or implied. See the License for the specific language governing 
 permissions and limitations under the License.
-
 */
 
 // This #include statement was automatically added by the Particle IDE.
 #include "RelayShield.h"
 
-// Create an instance of the RelayShield library, so we have something to talk to
+// create an instance of the RelayShield library for the valves
 RelayShield valveRelays;
+
+// create function methods and an array to track relay state
 int relayOn(String command);
 int relayOff(String command);
 int relayState[] = {0, 0, 0, 0, 0};
 
-STARTUP(WiFi.selectAntenna(ANT_EXTERNAL)); // selects the u.FL antenna
+// select the u.FL antenna
+STARTUP(WiFi.selectAntenna(ANT_EXTERNAL));
 
 void setup() {
-    //.begin() sets up a couple of things and is necessary to use the rest of the functions
     valveRelays.begin();
     Particle.function("relayOn", relayOn);
     Particle.function("relayOff", relayOff);
 
-    // setup the variables that we'll use for SmartThings to call in and check the valve status
     Particle.variable("valve1", &relayState[1], INT);
     Particle.variable("valve2", &relayState[2], INT);
     Particle.variable("valve3", &relayState[3], INT);
@@ -40,35 +39,39 @@ void setup() {
 }
 
 void loop() {
-    // Nothing needed here, functions will run when called
+    // nothing needed here, functions will run when called via the API
 }
 
+// define relayOn method
 int relayOn(String command){
     Particle.publish("Valve On", command);
-    // Ritual incantation to convert String into Int
+    
+    // convert char command to valve integer
     char inputStr[64];
     command.toCharArray(inputStr,64);
     int i = atoi(inputStr);
 
-    // Turn the desired relay on
+    // turn the desired relay on
     valveRelays.on(i);
     relayState[i] = 1;
 
-    // Respond
+    // respond when successful
     return 1;
 }
 
+// define relayOff method
 int relayOff(String command){
     Particle.publish("Valve Off", command);
-    // Ritual incantation to convert String into Int
+    
+    // convert char command to valve integer
     char inputStr[64];
     command.toCharArray(inputStr,64);
     int i = atoi(inputStr);
 
-    // Turn the desired relay off
+    // turn the desired relay off
     valveRelays.off(i);
     relayState[i] = 0;
 
-    // Respond
+    // respond when successful
     return 1;
 }
